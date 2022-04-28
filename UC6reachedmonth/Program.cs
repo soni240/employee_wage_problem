dc5b60c3b24ddc165a007c356e68dfc9d6837497
soni240 <s.fegade21@gmail.com>
2022-04-28 21:30:21 +0530

    reach month

--- UC6reachedmonth/Program.cs
+++ UC6reachedmonth/Program.cs
@@ -6,11 +6,13 @@ class Program
     public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
     public const int NUM_OF_WORKING_DAYS = 2;
+    public const int MAX_HRS_IN_MONTH = 10;
     static void Main(string[] args)
     {
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             switch (empCheck)
@@ -18,24 +20,21 @@ class Program
                 case IS_PART_TIME:
                     empHrs = 4;
                     break;
-
-
                 case IS_FULL_TIME:
                     empHrs = 8;
                     break;
                 default:
                     empHrs = 0;
                     break;
-
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            Console.WriteLine("Emp Wage :" + empWage);
-
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Day8:" + totalWorkingDays + " Emp Hrs: " + empHrs);
 
         }
-        Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Total Emp Wage:" + totalEmpWage);
     }
+
 
 
 
