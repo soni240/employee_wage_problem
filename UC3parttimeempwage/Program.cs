4d221b833e3876df1d6e6f9a52402c9f975449d9
soni240 <s.fegade21@gmail.com>
2022-04-28 21:21:57 +0530

    part time

--- UC3parttimeempwage/Program.cs
+++ UC3parttimeempwage/Program.cs
@@ -1,18 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
         Random random = new Random();
-        int empCheck = random.Next(0, 2);
-        if (empCheck == IS_FULL_TIME)
+        int empCheck = random.Next(0, 3);
+        if (empCheck == IS_PART_TIME)
+        {
+            empHrs = 4;
+        }
+        else if (empCheck == IS_FULL_TIME)
         {
             empHrs = 8;
+
         }
         else
         {
@@ -20,6 +27,6 @@ class Program
 
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Emp Wage : " + empWage);
+        Console.WriteLine("Emp Waage :" + empWage);
     }
 }
